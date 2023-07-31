1c6a540ce9856c7f777d8e16f7bdeead3d1b6c1c
TheCodeSword <mark.magrisy@yahoo.com>
2023-07-31 16:44:52 -0700

    a

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix substring
+ * @s: the substring
+ * @accept: the string to be compared
+ *
+ * Return: the number of bytes that exist in s and accept
+*/
+
+
