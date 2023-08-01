a834c03c4473c4a521ccc512f8375a227e6c2a72
TheCodeSword <mark.magrisy@yahoo.com>
2023-07-31 17:54:00 -0700

    a

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -1,19 +1,14 @@
+#include <unistd.h>
+#include "main.h"
+
 /**
- * main - Entry point
+ *  _putchar - writes the character c to stdout
+ * @c: The character to print
  *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (success)
+ * Return: On succes 1.
+ *              on error, -1 is returned and errno is set appropriately.
 */
-
-int main(void)
+int _putchar(char c)
 {
-        char str[] = "_putchar";
-        int ch;
-
-        for (ch = 0; ch < 8; ch++)
-                _putchar(str[ch]);
-        _putchar('\n');
-
-        return (0);
+        return (write(1, &c, 1));
 }
