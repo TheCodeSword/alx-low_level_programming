e8db793478038af23a7e63943c772e0fd1349f05
TheCodeSword <mark.magrisy@yahoo.com>
2023-07-31 17:04:37 -0700

    a

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -8,4 +8,17 @@
  * Return: the number of bytes that exist in s and accept
 */
 
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int itr, jtr;
 
+	for (itr = 0; s[itr] != '\0'; itr++)
+	{
+		for (jtr = 0; accept[jtr] != s[itr]; jtr++)
+		{
+			if (accept[jtr] == '\0')
+				return (itr);
+		}
+	}
+	return (itr);
+}
