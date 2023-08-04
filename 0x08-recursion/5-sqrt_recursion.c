f4b6b53027c5cd3c2b0d2451b30d49bcf9caded2
TheCodeSword <mark.magrisy@yahoo.com>
2023-08-03 17:06:05 -0700

    a

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,6 +1,25 @@
 #include "main.h"
 
 /**
+ * square - finds the roots of a number
+ * @n: the number
+ * @root: the root tested
+ *
+ * Return: the natural root of anumber if exists
+ *              -1 if it doesn't
+*/
+
+int square(int n, int root)
+{
+        if (root * root == n)
+                return (root);
+        else if (root * root < n)
+                return (square(n, root + 1));
+        else
+                return (-1);
+}
+
+/**
  * _sqrt_recursion - returns the natural square root of a number
  * @n: the number
  *
@@ -15,23 +34,4 @@ int _sqrt_recursion(int n)
 		return (0);
 	else
 		return (square(n, 1));
-}
-
-/**
- * square - finds the roots of a number
- * @n: the number
- * @root: the root tested
- *
- * Return: the natural root of anumber if exists
- *		-1 if it doesn't
-*/
-
-int square(int n, int root)
-{
-	if (root * root == n)
-		return (root);
-	else if (root * root < n)
-		return (square(n, root + 1));
-	else
-		return (-1);
 }
