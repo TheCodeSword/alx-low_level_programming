d50f8f1f23fcae1bdda193d51cd0087ec4b294d2
TheCodeSword <mark.magrisy@yahoo.com>
2023-08-03 17:28:46 -0700

    a

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -14,7 +14,7 @@ int test(int n, int f1, int f2)
 {
 	if (f2 > (n / 2))
 		return (1);
-	if (f1 * f2 == n)
+	if (f1 * f2 == n || n == 1 || n == 0)
 		return (0);
 	else if (f1 * f2 < n)
 		return (test(n, f1 + 1, f2));
