ea00b76cec476b5b2c7e889d0376f45a4f9da426
TheCodeSword <mark.magrisy@yahoo.com>
2023-08-08 12:38:21 -0700

    a

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,14 +22,18 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL && s2 == NULL)
 		return (NULL);
 	while (s1[size1] != '\0')
+	{
 		if (s1 == NULL)
 			break;
 		size1++;
+	}
 
 	while (s2[size2] != '\0')
+	{
 		if (s2 == NULL)
 			break;
 		size2++;
+	}
 	size = size1 + size2;
 
 	n = malloc(size * sizeof(*s1) + 1);
@@ -41,14 +45,18 @@ char *str_concat(char *s1, char *s2)
 	else
 	{
 		for (; i < size1; i++)
+		{
 			if (s1 == NULL)
 				break;
 			n[i] = s1[i];
+		}
 
 		for (; j < size2; j++)
+		{
 			if (s2 == NULL)
 				break;
 			n[i + j] = s2[j];
+		}
 	}
 	return (n);
 }
