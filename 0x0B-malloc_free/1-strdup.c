923eb73e3aa4695b6c11a00cb15b52ef643a4e0a
TheCodeSword <mark.magrisy@yahoo.com>
2023-08-08 12:09:44 -0700

    a

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -16,10 +16,13 @@ char *_strdup(char *str)
 	int size = 0, i = 0;
 	char *n;
 
+	if (str == NULL)
+		return (NULL);
+
 	while (str[size] != '\0')
 		size++;
 
-	n = malloc((size + 1) * sizeof(*str));
+	n = malloc(size * sizeof(*str) + 1);
 
 	if (size == 0 || n == 0)
 	{
