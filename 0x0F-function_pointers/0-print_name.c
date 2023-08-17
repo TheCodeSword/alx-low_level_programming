627279d98b0aacd603009853fbd8ab55168b4684
TheCodeSword <mark.magrisy@yahoo.com>
2023-08-16 18:59:20 -0700

    A

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name
+ * @name: the string to be printed
+ * @f: the printing function pointer
+ *
+ * Return: void
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name && f)
+		f(name);
+}
---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,20 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - returns pointer to right op func
+ * @s: string operator
+ *
+ * Return: the right function pointer
+*/
+
+int (*get_op_funv(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
